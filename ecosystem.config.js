be064b97947259922aa28743c00b32ea95d65866
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-10-30 16:29:05 +0700

    Bổ sung sync-manual

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: "my-app",
       script: "./dist/dist-sync-code-o-dh-012.js",
-      watch: ["./dist/dist-sync-code-o-dh-012.js"], // Chỉ giám sát file này
-      ignore_watch: ["*"], // Bỏ qua mọi thứ khác
+      watch: false,
+      // watch: ["./dist/dist-sync-code-o-dh-012.js"], // Chỉ giám sát file này
+      // ignore_watch: ["*"], // Bỏ qua mọi thứ khác
       //   max_restarts: 5,
       autorestart: true,
       env: {
